38d790cc839a27f7be519a00aac602b7c2fa5736
sonurao1 <56433890+sonurao1@users.noreply.github.com>
2025-01-06 19:14:15 +0530

    made /admin page for role admin

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const URL = require("../models/url");
+const User = require("../models/user")
 const { restrictTo } = require("../middlewares/auth");
 const router  = express.Router();
 
 
+router.get("/admin" , restrictTo(["ADMIN"]) ,  async (req , res) => {
+   const allUrls = await URL.find({})
+   const users = await Promise.all(allUrls.map(async (url) => User.find({ _id: url.createdBy })));
+   const allUsersName = users.map(user => user[0].name)
+  
+   console.log(allUsersName)
+    return res.render("home" , {
+        urls : allUrls,
+        role: "ADMIN",
+        usersName: allUsersName,
+    })
+})
 router.get("/" , restrictTo(["NORMAL" , "ADMIN"]) ,  async (req , res) => {
    const allUrls = await URL.find({createdBy: req.user?._id})
     return res.render("home" , {
